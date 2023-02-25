900784ce2be2cd13f7f7466033353bf9776cd513
PashaSPR <pashasobets@gmail.com>
2023-02-25 20:18:19 +0200

    First

---
+++ src/main/java/com/example/dr_19_02_2023_module_11_part_1_5_task/HelloServlet.java
@@ -0,0 +1,28 @@
+package com.example.dr_19_02_2023_module_11_part_1_5_task;
+
+import java.io.*;
+
+import jakarta.servlet.http.*;
+import jakarta.servlet.annotation.*;
+
+@WebServlet(name = "helloServlet", value = "/hello-servlet")
+public class HelloServlet extends HttpServlet {
+    private String message;
+
+    public void init() {
+        message = "Hello World!";
+    }
+
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        response.setContentType("text/html");
+
+        // Hello
+        PrintWriter out = response.getWriter();
+        out.println("<html><body>");
+        out.println("<h1>" + message + "</h1>");
+        out.println("</body></html>");
+    }
+
+    public void destroy() {
+    }
+}
